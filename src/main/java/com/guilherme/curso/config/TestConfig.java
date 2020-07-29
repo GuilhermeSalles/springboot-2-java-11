952d08de8b8a2f31b756ca8f0e538f9945ddd378
GuilhermeSalles <guibaltazarvs@gmail.com>
2020-07-29 14:31:58 -0300

    Payment, one-to-one association

--- src/main/java/com/guilherme/curso/config/TestConfig.java
+++ src/main/java/com/guilherme/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guilherme.curso.entities.Category;
 import com.guilherme.curso.entities.Order;
 import com.guilherme.curso.entities.OrderItem;
+import com.guilherme.curso.entities.Payment;
 import com.guilherme.curso.entities.Product;
 import com.guilherme.curso.entities.User;
 import com.guilherme.curso.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
