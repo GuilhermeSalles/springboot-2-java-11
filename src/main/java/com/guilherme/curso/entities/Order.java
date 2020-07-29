a914f78c0c19d93a261d81555c7fd917e3b918f6
GuilhermeSalles <guibaltazarvs@gmail.com>
2020-07-29 14:51:37 -0300

    Subtotal & Total methods and Payment Tidy

--- src/main/java/com/guilherme/curso/entities/Order.java
+++ src/main/java/com/guilherme/curso/entities/Order.java
@@ -30,8 +30,7 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	
+
 	private Integer orderStatus;
 
 	@ManyToOne
@@ -40,18 +39,19 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
+
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
 		this.id = id;
 		this.moment = moment;
-		this.client = client;
 		setOrderStatus(orderStatus);
+		this.client = client;
 	}
 
 	public Long getId() {
@@ -70,6 +70,16 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -86,20 +96,18 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
-	public OrderStatus getOrderStatus() {
-		return OrderStatus.valueOf(orderStatus);
-	}
-
-	public void setOrderStatus(OrderStatus orderStatus) {
-		if(orderStatus != null) {
-			this.orderStatus = orderStatus.getCode();
-		}
-	}
-
-	public Set<OrderItem> getItems(){
+	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -124,5 +132,4 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-
 }
